7bcc0f92cbeb445d572cae0af3e9339219d9bba9
Lung Chen <lungheichen@gmail.com>
2018-06-23 14:59:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ function viewCart() {
   if (!cart.length) {
     return 'Your shopping cart is empty.'
   } else if (cart.length == 1) {
-    return `In your cart, you have ${cart[0]["itemName"]} at ${cart[0]["itemPrice"]}.`
+    return `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}.`
   } else {
       lines = []
       for (var i = 0; i < cart.length; i++) {
         if (i < cart.length-1) {
-          lines.push(`${cart[i]["itemName"]} at ${cart[i]["itemPrice"]}`)
+          lines.push(`${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}`)
         } else {
-          lines.push(`and ${cart[i]["itemName"]} at ${cart[i]["itemPrice"]}.`)
+          lines.push(`and ${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}.`)
         }
       }
     return `In your cart, you have ${lines.join(', ')}`
