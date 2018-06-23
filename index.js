4f820ebbdd4906a65d50659257026201d33df389
Lung Chen <lungheichen@gmail.com>
2018-06-23 16:41:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    return "Sorry, we don't have a credit card on file for you")
+    return "Sorry, we don't have a credit card on file for you"
   } else {
     total = total()
     cart = []
