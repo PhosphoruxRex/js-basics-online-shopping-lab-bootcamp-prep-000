c30ad74624c7328c3a2d87bd5c051412d9dca514
Lung Chen <lungheichen@gmail.com>
2018-06-23 16:48:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    var tot = total();
+    var tot = total(cart);
     cart = []
     return `your total cost is $${tot}, which will be charged to the card ${cardNumber}.`
   }
