40d9a0753de29242c8d101bb46ba77b22d1a2efb
Lung Chen <lungheichen@gmail.com>
2018-06-23 16:38:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you")
+  } else {
+    
+  }
 }
 
 
