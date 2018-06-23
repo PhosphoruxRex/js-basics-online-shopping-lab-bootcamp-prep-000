770f37091704e0e115dcc7851048f1256349da18
Lung Chen <lungheichen@gmail.com>
2018-06-23 14:36:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var temp = {itemName: item, itemPrice: Math.floor((Math.random() * 100) + 1)}
+ cart.push(temp)
+ return `${temp[item]} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (!cart.length) {
+    return 'Your shopping cart is empty.'
+  } else {
+      lines = []
+      for (var i = 0; i < cart.length; i++) {
+        if (i < cart.length-1) {
+          lines.push(`${cart[i][itemName]} at ${cart[i][itemPrice]}`)
+        } else {
+          lines.push(`and ${cart[i][itemName]} at ${cart[i][itemPrice]}.`)
+        }
+      }
+    return `In your cart, you have ${lines.join(', ')}`
+  }
+
 }
 
 function total() {
-  // write your code here
+  total = 0
+  if (!cart.length) {
+    return total
+  } else {
+
+    for (var i = 0; i < cart.length; i++) {
+      total += parseInt(cart[i][itemPrice])
+    }
+    return total
+  }
 }
 
 function removeFromCart(item) {
@@ -28,3 +52,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 }
+
+
+
+// shift(item) unshift()
+// push(item) pop()
