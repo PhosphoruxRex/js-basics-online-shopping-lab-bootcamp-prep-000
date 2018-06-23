b1ef519442c8b424baf1180a1d3dde7944cd1a3f
Lung Chen <lungheichen@gmail.com>
2018-06-23 15:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,10 +53,9 @@ function removeFromCart(item) {
     if (cart[i].itemName == item) {
       cart = cart.splice(i, 1)
       return cart
-    } else {
-      return 'That item is not in your cart'
     }
   }
+  return 'That item is not in your cart'
 }
 
 function placeOrder(cardNumber) {
