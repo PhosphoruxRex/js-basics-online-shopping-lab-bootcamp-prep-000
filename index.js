8fa13b610fb16fab0736d37f03f6414d917f9fc6
Lung Chen <lungheichen@gmail.com>
2018-06-23 16:36:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
     for (var i = 0; i < cart.length; i++) {
       console.log('why nothing happens')
       if (cart[i].itemName == item) {
-        cart = cart.splice(i, 1)
+        cart.splice(i, 1)
         return cart
       }
     }
