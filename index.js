302c2be76040698c6a27d27c978cf446bce3e7a4
Lung Chen <lungheichen@gmail.com>
2018-06-23 14:46:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var temp = {itemName: [item], itemPrice: Math.floor((Math.random() * 100) + 1)}
+  var temp = {itemName: item, itemPrice: Math.floor((Math.random() * 100) + 1)}
   cart.push(temp)
   return `${temp[item]} has been added to your cart.`
 }
